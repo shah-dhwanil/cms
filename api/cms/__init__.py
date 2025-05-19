8c4c214fd0f9001850927bc918e345a17f95ea36
shahprogrammer05 <shahprogrammer05@gmail.com>
2025-05-19 13:57:33 +0000

    Created Config class to manage configurations

--- api/cms/__init__.py
+++ api/cms/__init__.py
@@ -1,2 +1,5 @@
+from cms.utils.config import Config
 def main() -> None:
+    Config.load_config()
+    print(Config.get_config())
     print("Hello from cms!")
---
+++ api/cms/utils/config.py
@@ -0,0 +1,37 @@
+from typing import Self
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from tomllib import load
+from os import environ
+
+__config__ = None
+
+class Config(BaseModel):
+    SERVER_ENVIRONMENT:str
+    SERVER_HOST:str
+    SERVER_PORT:int
+
+    @classmethod
+    def load_config(cls):
+        load_dotenv("./.env")
+        environment = environ.get("CMS_SERVER_ENVIRONMENT",None)
+        if environment is None:
+            raise ValueError("Server Environment must be set in the system")
+        load_dotenv(f".env.{environment.lower()}")
+        config = dict()
+        with open("./config.toml", "rb") as f:
+            toml_config = load(f)
+            config.update(toml_config.get("DEFAULT",dict()))
+            config.update(toml_config.get(environment,dict()))
+        for key in environ:
+            if key.startswith("CMS_"):
+                config[key.removeprefix("CMS_")] = environ[key]
+        global __config__
+        __config__ = cls(**config)
+
+    @classmethod
+    def get_config(cls)->Self:
+        global __config__
+        if __config__ is None:
+            raise Exception("Config is not loaded")
+        return __config__
