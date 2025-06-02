d277cee2c614307f9843e7665e9096276e2ab7df
shahprogrammer05 <shahprogrammer05@gmail.com>
2025-06-02 07:21:58 +0000

    Updated app.py

--- api/cms/app/app.py
+++ api/cms/app/app.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from cms.app.lifespan import lifespan
 from structlog import get_logger
-app = FastAPI(title="College Management System",lifespan=lifespan)
-
+from cms.utils.minio import MinioClient
+app = FastAPI(title="College Management System", lifespan=lifespan)
 @app.get("/")
 async def root():
+    print("request_recieved")
+    client = MinioClient.get_client()
+    await client.make_bucket("firebase")
     get_logger().info("Welcome to College Management System")
     return {"message": "Welcome to College Management System"}
