1d5c995a84e0a056e01d9872e57ecfd3761ee98f
shahprogrammer05 <shahprogrammer05@gmail.com>
2025-06-02 13:16:47 +0000

    Created BaseException class

---
+++ api/cms/utils/exceptions.py
@@ -0,0 +1,11 @@
+from typing import Any, ClassVar
+
+
+
+class BaseException(Exception):
+    slug: ClassVar[str]
+    description: ClassVar[str]
+
+    def __init__(self, context: dict[str, Any] = dict(), *args) -> None:
+        self.context = context
+        super().__init__(*args)
